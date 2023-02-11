732ac1e8b2dc022c37cfad666c993464d118ad0e
aimireal <dylanhudson1998@gmail.com>
2023-02-11 00:49:20 +0000

    Tailwind and TS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
