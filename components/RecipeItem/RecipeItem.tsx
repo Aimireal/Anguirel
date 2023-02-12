a00e24e137625f41851e7af775ded91187f0283a
aimireal <dylanhudson1998@gmail.com>
2023-02-12 19:40:17 +0000

    Added an SVG background to the site

--- components/RecipeItem/RecipeItem.tsx
+++ components/RecipeItem/RecipeItem.tsx
@@ -13,40 +13,46 @@ export function RecipeItem(recipe: RecipeType) {
 	const hasDetails = Object.hasOwn(recipe, "details");
 	const renderedDetails = () => {
 		return recipe.details?.map((detail) => {
-			return <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">{detail.text}</p>;
+			return (
+				<p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+					{detail.text}
+				</p>
+			);
 		});
 	};
 
 	return (
-		<div className="max-w-sm mx-auto bg-white border rounded-lg shadow dark:bg-gray-800">
-			<a href="#">
-				<img className="rounded-t-lg" src={recipe.image} alt={recipe.title} />
-			</a>
-			<div className="p-5">
-				<a href="#">
-					<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+		<div className="my-8 rounded shadow-lg shadow-gray-200 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-300 hover:-translate-y-1">
+			<a href={recipe.slug} className="cursor-pointer">
+				<img
+					className="rounded-t h-72 w-full object-cover"
+					src={recipe.image}
+					alt={recipe.title}
+				/>
+				<figcaption className="p-4">
+					<p className="text-lg mb-4 font-bold leading-relaxed text-gray-900 dark:text-white">
 						{recipe.title}
-					</h5>
-				</a>
-				<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-					{recipe.description}
-				</p>
+					</p>
+					<small className="leading-5 text-gray-700 dark:text-gray-400">
+						{recipe.description}
+					</small>
+				</figcaption>
 				{hasDetails === true ? null : (
-					<button
-						onClick={onNavigate}
-						className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-					>
-						Show Recipe
-					</button>
+					<div className="flex justify-center">
+						<button
+							onClick={onNavigate}
+							className="inline-flex items-center px-3 py-2 mb-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+						>
+							Show Recipe
+						</button>
+					</div>
 				)}
 				{hasDetails === true ? (
 					<Fragment>
-						<div>
-							{renderedDetails()}
-						</div>
+						<div>{renderedDetails()}</div>
 					</Fragment>
 				) : null}
-			</div>
+			</a>
 		</div>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Roboto', 'sans-serif']
-      }
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./app/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Roboto", "sans-serif"],
+			},
+			backgroundImage: {
+				pattern: "url('../public/background.svg)",
+			},
+		},
+	},
+	plugins: [],
+};
+
