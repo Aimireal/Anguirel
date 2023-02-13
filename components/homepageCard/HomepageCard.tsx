dc6cba057a510fcf969c54382c65400607758b7f
aimireal <dylanhudson1998@gmail.com>
2023-02-13 20:51:54 +0000

    Homepage Card Font

--- components/homepageCard/HomepageCard.tsx
+++ components/homepageCard/HomepageCard.tsx
@@ -2,29 +2,35 @@ function HomepageCard() {
 	return (
 		<section className="pt-4 flex items-center justify-center py-8 mx-12 rounded bg-white dark:bg-gray-800">
 			<div className="max-w-4xl mx-auto text-center">
-				<h2 className="mx-2 text-1xl font-extrabold leading-9 text-white sm:text-2xl sm:leading-10">
+				<h2 className="mx-2 text-1xl sm:text-base font-extrabold leading-9 text-white sm:leading-10">
 					Used by professional and home bakers, everywhere
 				</h2>
-				<p className="mx-2 mt-2 text-base leading-7 text-white">
+				<p className="mx-2 mt-2 text-base sm:text-sm leading-7 text-white">
 					Feel confident in using this selection of tried and true recipes
 				</p>
 			</div>
-			<div className="mx-2 mt-4 text-center sm:max-w-2xl sm:mx-auto sm:grid sm:grid-cols-3 sm:gap-8">
+			<div className="mx-2 mt-4 text-center sm:text-sm sm:max-w-2xl sm:grid sm:grid-cols-3 sm:gap-8">
 				<div>
-					<p className="text-3xl font-extrabold leading-none text-white">8</p>
-					<p className="mt-2 text-base font-medium leading-6 text-white">
+					<p className="text-3xl sm:text-base font-extrabold leading-none text-white">
+						8
+					</p>
+					<p className="mt-2 text-base sm:text-sm font-medium leading-6 text-white">
 						Recipes
 					</p>
 				</div>
 				<div className="mx-4 mt-4 sm:mt-0">
-					<p className="text-3xl font-extrabold leading-none text-white">4</p>
-					<p className="mt-2 text-base font-medium leading-6 text-white">
+					<p className="text-3xl sm:text-base font-extrabold leading-none text-white">
+						4
+					</p>
+					<p className="mt-2 text-base sm:text-sm font-medium leading-6 text-white">
 						Categories
 					</p>
 				</div>
 				<div className="mx-4 mt-4 sm:mt-0">
-					<p className="text-3xl font-extrabold leading-none text-white">1</p>
-					<p className="mt-2 text-base font-medium leading-6 text-white">
+					<p className="text-3xl sm:text-base font-extrabold leading-none text-white">
+						1
+					</p>
+					<p className="mt-2 text-base sm:text-sm font-medium leading-6 text-white">
 						User
 					</p>
 				</div>
