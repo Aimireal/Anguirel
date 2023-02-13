7ccb5db7097fe6f4adbe39fd0ecb30ff13aac59e
aimireal <dylanhudson1998@gmail.com>
2023-02-13 22:24:58 +0000

    Type for recipe ingredients

--- types/RecipeType.tsx
+++ types/RecipeType.tsx
@@ -5,9 +5,16 @@ export type RecipeType = {
 	image: string;
 	description: string;
 	details?: RecipeDetails[];
+	ingredients?: IngredientsType[];
 };
 
 export type RecipeDetails = {
     id: number;
     text: string;
+}
+
+export type IngredientsType = {
+	category?: string;
+	text: string;
+	quantity: string;
 }
