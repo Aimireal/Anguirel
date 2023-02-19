b52dd5fdc562b285a22bf149681c4d88d7829b61
aimireal <dylanhudson1998@gmail.com>
2023-02-19 12:19:24 +0000

    Image encoded as base64

--- utils/UploadImage.tsx
+++ utils/UploadImage.tsx
@@ -1,16 +1,34 @@
 import axios, { AxiosResponse } from "axios";
 
+function fileToBase64(file: File): Promise<string | ArrayBuffer | null> {
+	return new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = () => {
+			const base64String = reader.result;
+			resolve(base64String);
+		};
+		reader.onerror = (error) => reject(error);
+	});
+}
+
 export async function UploadImage(imageFile: File): Promise<string> {
 	const imgurId = "0f4d305139ce00e";
 	const formData = new FormData();
-	formData.append("image", imageFile);
 
-	const response = await axios.post("https://api.imgur.com/3/upload", {
-		headers: {
-			Authorization: `Client-ID ${imgurId}`,
-		},
-		body: formData,
-	});
-	
-	return response.data.data.link;
+	const base64Image = await fileToBase64(imageFile);
+
+	if (base64Image !== null) {
+		formData.append("image", base64Image?.toString());
+
+		const response = await axios.post("https://api.imgur.com/3/upload", {
+			headers: {
+				Authorization: `Client-ID ${imgurId}`,
+			},
+			body: formData,
+		});
+
+		return response.data.data.link;
+	}
+	return "Error";
 }
