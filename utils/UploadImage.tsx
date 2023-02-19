a8ca4352a3a3836b072a811288e27c3b3c3fd95a
aimireal <dylanhudson1998@gmail.com>
2023-02-19 11:34:53 +0000

    Test imgur connection

--- utils/UploadImage.tsx
+++ utils/UploadImage.tsx
@@ -1,12 +1,13 @@
 import axios, { AxiosResponse } from "axios";
 
 export async function UploadImage(imageFile: File): Promise<string> {
+	const imgurId = "0f4d305139ce00e";
 	const formData = new FormData();
 	formData.append("image", imageFile);
 
 	const response = await axios.post("https://api.imgur.com/3/image", {
 		headers: {
-			Authorization: `Client-ID ${process.env.IMGUR_CLIENT_ID ?? ""}`,
+			Authorization: `Client-ID ${imgurId}`,
 		},
 		body: formData,
 	});
