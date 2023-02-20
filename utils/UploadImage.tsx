497da1e41c155049305af6f6f70e3281212b797e
aimireal <dylanhudson1998@gmail.com>
2023-02-20 22:13:36 +0000

    Upload TryEnv

--- utils/UploadImage.tsx
+++ utils/UploadImage.tsx
@@ -1,18 +1,17 @@
 import axios from "axios";
 
-const clientId = process.env.IMGUR_CLIENT_ID;
-
 export async function UploadImage(imageFile: File): Promise<string> {
+	const clientId = process.env.IMGUR_CLIENT_ID;
 	var formData = new FormData();
 	formData.append("image", imageFile);
 
 	try {
 		const response = await axios.post(
-			"https://api.imgur.com/3/image",
+			"https://api.imgur.com/3/upload",
 			formData,
 			{
 				headers: {
-					Authorization: "Client-ID " + clientId,
+					Authorization: "Client-ID 0f4d305139ce00e",
 				},
 			}
 		);
